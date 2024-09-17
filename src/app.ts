3097d2b8c99cb1e84d6c04a2e9d35bb52f770d94
dev-moriai <bdevatmoriai@gmail.com>
2024-09-17 18:08:59 +0330

    manager app.ts copy added

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,5 @@
 
-import express from 'express';
-import connectDB from './config/db';
-import ticketRoutes from './routes/ticket';
-import dotenv from 'dotenv';
+import express, { Response, Request } from 'express';
 const cors = require('cors');
 
 
@@ -14,53 +11,54 @@ declare global {
       }
 }
 
-dotenv.config();
- 
-      console.log(process.env.MONGO_URI)
-      const app = express();
-      const PORT = process.env.PORT || 5000;
 
-      connectDB();
+const app = express();
+const PORT = 5000;
 
-      app.use(express.json());
+
+app.use(express.json());
 
 
 
 
-      app.use(function (req, res, next) {
-            console.log("origin:")
-            console.log(req.headers.origin)
-            const origin = req.headers.origin;
+app.use(function (req, res, next) {
+      console.log("origin:")
+      console.log(req.headers.origin)
+      const origin = req.headers.origin;
 
-            if (req.headers.origin == 'https://melarix.com') {
-                  app.use(cors({
-                        origin: 'https://melarix.com'
-                  }));
-            }
-            if (req.headers.origin == 'http://127.0.0.1:8080') {
-                  app.use(cors({
-                        origin: 'http://127.0.0.1:8080'
-                  }));
-            }
+      if (req.headers.origin == 'https://melarix.com') {
+            app.use(cors({
+                  origin: 'https://melarix.com'
+            }));
+      }
+      if (req.headers.origin == 'http://127.0.0.1:8080') {
+            app.use(cors({
+                  origin: 'http://127.0.0.1:8080'
+            }));
+      }
 
-            res.header("Access-Control-Allow-Origin", origin);
-            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-            next();
+      res.header("Access-Control-Allow-Origin", origin);
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      next();
 
-      });
+});
 
-      // just for localdev test
-      app.use(cors({ origin: ["http://localhost:3000", "http://127.0.0.1:8080"] }));
+// just for localdev test
+app.use(cors({ origin: ["http://localhost:3000", "http://127.0.0.1:8080"] }));
+const router = express.Router();
 
+router.get('/test', async (req: Request, res: Response) => {
+      console.log('test is called')
+      try {
 
+            res.status(201).json({ status: "success", msg: "test is ok on the ts " });
+      } catch (err) {
+            res.status(500).json({ msg: 'Server Error' });
+      }
+});
+app.use('', router);
 
+app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+});
 
-
-
-      app.use('', ticketRoutes);
-
-      app.listen(PORT, () => {
-            console.log(`Server running on port ${PORT}`);
-      });
-
- 
